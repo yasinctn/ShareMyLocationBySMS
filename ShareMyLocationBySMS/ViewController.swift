b75e52643c355d76d71a33a411e523a34b917d5d
Yasin Cetin <yasincetin2014@gmail.com>
2025-05-12 13:25:38 +0300

    Initial Commit

---
+++ ShareMyLocationBySMS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShareMyLocationBySMS
+//
+//  Created by Yasin Cetin on 12.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
